766f4bd5b1a947d7b64c9dc27f5fe22792297065
GustavoM7 <gustavosmarquesf@gmail.com>
2018-07-08 21:19:47 -0300

    Criando js integrado, adicionando animações ao css principal.

---
+++ jsfinal.js
@@ -0,0 +1,33 @@
+//'hover' do rodapé
+
+function hoverfb() {
+	document.getElementById("face").src="iconefbhover.png";
+}
+
+function normalizefb() {
+	document.getElementById("face").src="iconefb.png";	
+}
+
+function hovertwitter() {
+	document.getElementById("twitter").src="iconetwthover.png";
+}
+
+function normalizetwitter() {
+	document.getElementById("twitter").src="iconetwt.png";	
+}
+
+function hoverinstagram() {
+	document.getElementById("instagram").src="iconeinsthover.png";
+}
+
+function normalizeinstagram() {
+	document.getElementById("instagram").src="iconeinst.png";	
+}
+
+function hoveryt() {
+	document.getElementById("youtube").src="iconeythover.png";
+}
+
+function normalizeyt() {
+	document.getElementById("youtube").src="iconeyt.png";	
+}
